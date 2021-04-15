2573a164d5cf21f4148d87e597afe6025eb630e8
lingi524 <lingi524@student.liu.se>
2021-04-15 17:00:20 +0200

    Now it also changes the minutes every time the seconds get to 0 (or gets to be smaller than 1). But its also stuck in an infite loop. Great

--- src/Clock.js
+++ src/Clock.js
@@ -5,17 +5,19 @@ function Clock () {
     let minutes = 25;
     let seconds = 59;
 
-    timer = setInterval(
-    function looping() {
-        if (seconds>=1) {
-            seconds--;
-        } else if (seconds<1) {
-            minutes--;
-        } else if (minutes<1){
-            alert("Done for now");
-        }
+    var timer = setInterval(
+        function looping() {
+            if (seconds>=1) {
+                console.log(minutes+"Minutes"+ seconds-- +"Seconds");
+            } else if (seconds<1 && minutes<1) {
+                console.log(minutes-- +"Minutes"+ seconds +"Seconds");
+                seconds = 59;
+            } else if (minutes<1){
+                alert("Done for now");
+            }
+    },100);
+ 
 
-},1000);
 return (
     <div className="Clock">
         <h2>Its: {minutes} minutes and {seconds} seconds left</h2>
