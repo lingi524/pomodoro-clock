fc9b8ffe2ba8091a840704cc216d336c0a98038a
lingi524 <lingi524@student.liu.se>
2021-04-16 17:03:03 +0200

    Timer works, tells you that its breaktime, and starts a break timer. Need to fix the breaktime alert.

--- src/Clock.js
+++ src/Clock.js
@@ -2,9 +2,16 @@ import React, {useState, useEffect} from "react";
 import './App.css';
 
 function Clock () {
-    const [isActive, setIsActive] = useState(false);
-    let [minutes, setMinutes] = useState(10);
-    let [seconds, setSeconds] = useState(59);
+    let [isActive, setIsActive] = useState(false);
+    let [minutes, setMinutes] = useState(1);
+    let [seconds, setSeconds] = useState(0);
+    let [isBreakTime, setIsBreakTime] = useState(false);
+
+    function resetTimer() {
+        setMinutes(25);
+        setSeconds(0);
+        setIsActive(false);
+      }
 
     useEffect(()=>{
     if (isActive){
@@ -12,21 +19,43 @@ function Clock () {
             var interval = setInterval(() => {
                 setSeconds(seconds-1);
             } ,1000) }
-            else if (seconds<1 && minutes>1) {
+            else if (seconds<1 && minutes>=1) {
                 setMinutes(minutes-1) ;
                 setSeconds(seconds = 59);
 
-            } else if (minutes<1){
-                alert("Done for now");
+            } else if (minutes<1&&seconds===0){
+                alert("Done for now, time for break");
+                isBreakTime = true;
             }
     } return ()=> {clearInterval(interval)}
 },[isActive, seconds, minutes]);
+
+useEffect(()=>{
+    if (isBreakTime){
+
+            setMinutes(5);
+            setSeconds(0);
+
+            if (seconds>=1) {
+            var interval = setInterval(() => {
+                setSeconds(seconds-1);
+            } ,1000) }
+            else if (seconds<1 && minutes>=1) {
+                setMinutes(minutes-1) ;
+                setSeconds(seconds = 59);
+
+            } else if (minutes<1&&seconds===0){
+                alert("Break is up"); //Fix so this is not the first thing that pops up when the if statement runs
+            }
+    } return ()=> {clearInterval(interval)}
+},[isBreakTime, seconds, minutes]);
  
 
 return (
         <div className="Clock">
             <h2>Its: {minutes} minutes and {seconds} seconds left</h2>
-            <button onClick={()=> setIsActive(!isActive)} className="start">Start Timer</button>
+            <button onClick={()=> setIsActive(!isActive)} className="start">{isActive? "Pause":"Start Timer"}</button>
+            <button style={{display: isActive ? "" : 'none' }} onClick={resetTimer}>Reset</button>
         </div>
     ) 
 }
